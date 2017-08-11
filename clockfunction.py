a688aa273291b227acfe817e481b357ab59b02dc
Dima Kogan <dima@secretsauce.net>
2017-08-11 00:20:54 -0700

    comments

--- clockfunction.py
+++ clockfunction.py
@@ -9,7 +9,7 @@ import numpy as np
 import subprocess
 
 r'''This is a simple tool to run an application under perf to measure the runtime
-of given functions'''
+of given functions. Invoke with no arguments to get a useful usage message'''
 
 contexts = {}
 perf     = "perf"
@@ -181,10 +181,46 @@ if __name__ == '__main__':
         exe_link = os.readlink("/proc/{}/exe".format(pid) )
         if not re.match( "perf(_.*)?$", os.path.basename( exe_link )):
 
-                usage = "{} func@lib [func@lib ...] cmd arg0 arg1 arg2 ..."
+                usage = "Usage: {} func@lib [func@lib ...] cmd arg0 arg1 arg2 ...\n".format(sys.argv[0]) + \
+r"""
+This program runs an arbitrary application with arbitrary args (cmd arg0 arg1
+arg2 ...) with some (very light) instrumentation. This instrumentation traces
+entries and exits to a given set of functions, and measures the elapsed time
+spent. When the application exits, basic statistics are printed to let the user
+know how fast or slow the queried functions are.
+
+Each function is specified as a 'func@lib' string (ltrace-style). 'func' is the
+name of the function we care about. This could be a shell pattern to pick out
+multiple functions. 'lib' is the ELF library or executable that contains this
+function; must be an absolute path.
+
+Example:
+
+  # function mean min max stdev Ncalls
+  ## All timings in seconds
+  Perl_drand48_init_r 7.55896326154e-06 7.55896326154e-06 7.55896326154e-06 0.0               1
+  Perl_drand48_r      1.95271501819e-06 1.76404137164e-06 3.67719912902e-05 4.0105865074e-07  100001
+  Perl_pp_rand        5.23026800056e-06 4.78199217469e-06 0.000326015986502 1.71576428687e-06 100001
+  perl_run            0.662568764063    0.662568764063    0.662568764063    0.0               1
+
+The table was re-spaced for readability. So we see that the main perl
+application took 0.66 seconds. And Perl_pp_rand was called 100001 times, taking
+5.23us each time, on average, for a total of 0.523 seconds. A lower-level
+Perl_drand48_r function is about 1/3 of the time of Perl_pp_rand. If one cared
+about this detail of perl, this would be very interesting to know. And we found
+it out without any compile-time instrumentation of our binary.
+
+Recursive or parallel invocations are supported so far as the mean and Ncalls
+will be reported correctly. The min, max and stdev of the timings will not be
+available, however.
+
+This tool is a quick hack, and all the actual work is done by 'perf'. This tool
+calls 'sudo' all over the place, which is ugly.
+
+"""
 
                 if len(sys.argv) < 3:
-                        print "Usage: " + usage.format(sys.argv[0])
+                        print usage
                         sys.exit(1)
 
                 # command is the first argument without a single '@'
